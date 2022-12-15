97a6df037beeab223948805edf9ee74385572648
Júlio Freitas <jcsr.frts@gmail.com>
2022-12-15 10:21:06 -0300

    feat: create table courses component

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import { Course } from './../model/course.interface';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-
+  public displayedColumns = ['name', 'category'];
+  public courses: Course[] = [
+    {
+      _id: '1',
+      name: 'Crud Angular + Spring',
+      category: 'Fullstack',
+    },
+    {
+      _id: '2',
+      name: 'Angular - Loiane',
+      category: 'Frontend',
+    },
+  ];
 }
