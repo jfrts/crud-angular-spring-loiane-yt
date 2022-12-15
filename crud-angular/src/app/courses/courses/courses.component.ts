c968ea4939b381283383bf8dca734ac0d11bdbf8
Júlio Freitas <jcsr.frts@gmail.com>
2022-12-15 20:01:16 -0300

    feat: create error dialog component

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
 
+import { ErrorDialogComponent, IError } from './../../shared/components/error-dialog/error-dialog.component';
 import { Course } from './../model/course.interface';
 import { CourseService } from './../services/course.service';
 
@@ -13,7 +15,19 @@ export class CoursesComponent {
   public displayedColumns = ['name', 'category'];
   public courses$: Observable<Course[]>;
 
-  constructor(private coursesService: CourseService) {
-    this.courses$ = this.coursesService.list();
+  constructor(private coursesService: CourseService, public dialog: MatDialog) {
+    this.courses$ = this.coursesService.list().pipe(
+      catchError((error) => {
+        this.onError({
+          errorTitle: 'An error was found',
+          errorMessage: 'Error loading courses',
+        });
+        return of([]);
+      })
+    );
+  }
+
+  onError(data: IError) {
+    this.dialog.open(ErrorDialogComponent, { data });
   }
 }
--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  exports: [MatTableModule, MatToolbarModule, MatProgressSpinnerModule],
+  exports: [
+    MatTableModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule
+  ],
 })
 export class AppMaterialModule {}
